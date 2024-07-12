3654c90ca5d8809635dcc65980c0c9894150e91c
soelz4-artix-runit <soheilzolfaghari78@gmail.com>
2024-07-12 10:25:26 +0330

    src/services/stock/store.go Updated

--- src/services/stock/store.go
+++ src/services/stock/store.go
@@ -2,10 +2,12 @@ package stock
 
 import "database/sql"
 
+// Stock Storage
 type Store struct {
 	db *sql.DB
 }
 
+// Return New Stock Storage
 func NewStore(db *sql.DB) *Store {
 	return &Store{db: db}
 }
